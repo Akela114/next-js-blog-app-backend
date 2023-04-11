8b1ec629c4189780cef42ed2b09d60897007ef8c
Oleg Nikolaev <o.nikolaev@iq-adv.ru>
2023-04-11 20:47:16 +0500

    fix: add headers

--- api/server.js
+++ api/server.js
@@ -3,6 +3,17 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+const cors = require('cors')
+
+server.use(
+  cors({
+    origin: true,
+    credentials: true,
+    preflightContinue: false,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  })
+)
+server.options('*', cors())
 
 server.use(middlewares)
 // Add this before server.use(router)
